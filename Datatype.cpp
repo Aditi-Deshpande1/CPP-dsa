cbe2af4beb66ff06dce42b2969aa3be07e242df0
Aditi Deshpande <aditidesh2007@gmail.com>
2025-09-25 06:36:00 +0530

    day1

---
+++ Datatype.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    cout<<a<<endl;
+
+    char b = 'A';
+    cout<<b<<endl;
+
+    bool bl = true;
+    cout<<bl<<endl;
+
+    float f = 10.5;
+    cout<<f<<endl;
+
+    double d = 20.99;
+    cout<<d<<endl;
+
+    int size = sizeof(b);
+    cout<< "Size of b : "<<size<<endl;
+
+}
